721747731c682bf6d43a86171eee4f2b2c7d1d4c
chickensalad303 <miloiversenginsburg@gmail.com>
2024-02-04 12:08:05 +0100

    run test.py to generate spotipy cache

---
+++ frontend/test.py
@@ -0,0 +1,27 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+scope = "user-follow-read," \
+        "user-library-read," \
+        "user-library-modify," \
+        "user-modify-playback-state," \
+        "user-read-playback-state," \
+        "user-read-currently-playing," \
+        "app-remote-control," \
+        "playlist-read-private," \
+        "playlist-read-collaborative," \
+        "playlist-modify-public," \
+        "playlist-modify-private," \
+        "streaming"
+
+clientid = "ab89fb3e5529467c91a245ce5b69067c"
+clientsecret = "b123ce33e1d04bfaafeefbcad9529414"
+redirectURI = "http://localhost:8080/"
+# spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        scope=scope, 
+        client_id=clientid, client_secret=clientsecret, redirect_uri=redirectURI))
+
+results = sp.current_user_saved_tracks(limit=50, offset=0)
+print(results)
