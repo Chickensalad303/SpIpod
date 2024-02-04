763c8cf46410835a64fb217c5924d296efc8f266
Chickensalad303 <91982781+Chickensalad303@users.noreply.github.com>
2024-02-05 00:27:33 +0100

    Update test.py

---
+++ frontend/test.py
@@ -0,0 +1,37 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from sys import platform
+import distro
+
+
+print(distro.id())
+
+scope = "user-follow-read," \
+        "user-library-read," \
+        "user-library-modify," \
+        "user-modify-playback-state," \
+        "user-read-playback-state," \
+        "user-read-currently-playing," \
+        "app-remote-control," \
+        "playlist-read-private," \
+        "playlist-read-collaborative," \
+        "playlist-modify-public," \
+        "playlist-modify-private," \
+        "streaming"
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+clientID = os.getenv("clientID")
+clientSecret = os.getenv("clientSecret")
+redirectURI = os.getenv("redirectURI")
+
+# spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        scope=scope, 
+        client_id=clientid, client_secret=clientsecret, redirect_uri=redirectURI))
+
+results = sp.current_user_saved_tracks(limit=50, offset=0)
+print(results)
